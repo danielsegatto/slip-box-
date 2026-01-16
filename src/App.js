1b1ee25613c7713afa4b10ec3ff770529552886f
Daniel Segatto <41549713+danielsegatto@users.noreply.github.com>
2026-01-16 14:51:47 -0300

    Initialize React app with basic structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World!</h1>
+      <p>My React app is running 100% online.</p>
+    </div>
+  );
+}
+
+export default App;
